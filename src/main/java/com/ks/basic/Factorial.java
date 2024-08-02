a648dfb019fb2c5c0d0deacc57e55245668e5633
deepak kr <imt.deepak@gmail.com>
2024-08-01 18:23:42 -0700

    Binary Search Lower Bound

--- src/main/java/com/ks/basic/Factorial.java
+++ src/main/java/com/ks/basic/Factorial.java
@@ -1,16 +1,15 @@
 package com.ks.basic;
 
 /**
- * @author
+ *
  */
 public class Factorial {
 
     public static void main(String args[]) {
-        Factorial factorial = new Factorial();
-        factorial.calculateFactorial(7);
+        calculateFactorial(7);
     }
 
-    public void calculateFactorial(int n){
+    public static void calculateFactorial(int n){
         int result  = 1;
         for(int i = n; i >= 1; i--) {
             result = result * i;
---
+++ src/main/java/com/ks/datastructures/binarysearch/LowerBound.java
@@ -0,0 +1,55 @@
+package com.ks.datastructures.binarysearch;
+
+import java.util.Arrays;
+
+public class LowerBound {
+    public static void main(String[] args){
+        int[] arr = { 2, 3, 5, 7, 8, 9, 10, 15, 18, 20, 25};
+        System.out.println(String.format("Array Provided : %s", Arrays.toString(arr)));
+        System.out.println(lowerBound(arr, 11));
+        System.out.println(lowerBound(arr, 19));
+        System.out.println(lowerBound(arr, 6));
+        System.out.println(lowerBound(arr, 27));
+
+
+
+        System.out.println(lowerBoundBi(arr, 11));
+        System.out.println(lowerBoundBi(arr, 19));
+        System.out.println(lowerBoundBi(arr, 6));
+        System.out.println(lowerBoundBi(arr, 27));
+
+    }
+
+    private static int lowerBound(int[] arr, int key) {
+        System.out.println(String.format("Find Lower bound for %d", key));
+        int low = 0, high = arr.length;
+        while (low < high) {
+            int mid = low + (high - low)/2;
+            if(arr[mid] >= key) {
+                high = mid;
+            } else {
+                low = mid +1;
+            }
+        }
+        if (low <arr.length && key > arr[low] ) {
+            low++;
+        }
+        System.out.println("-------------------------------------------");
+        return low;
+    }
+    private static int lowerBoundBi(int[] arr, int key) {
+        System.out.println(String.format("Find Lower bound for %d", key));
+        int low = 0, high = arr.length;
+        int ans = high;
+        while (low<high) {
+            int mid = (low + high)/2;
+            if(arr[mid] >= key) {
+                ans = mid;
+                high = mid -1;
+            } else {
+                low = mid +1;
+            }
+        }
+        return ans;
+    }
+}
